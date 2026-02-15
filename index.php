47b52ea12aac7d1fc6bae02d7c553969645c56b6
nikhilgoku8 <nikhilgoku8@gmail.com>
2026-02-15 22:56:43 +0530

    Initial

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+<title>Nutrition</title>
+<meta name="description" content="," />
+<meta name="keywords" content="" />
+
+<link href="css/reset.css" rel="stylesheet" type="text/css" />
+<link href="css/ace-responsive-menu.css" rel="stylesheet" type="text/css" />
+
+<link href="css/animate.4.1.1.css" rel="stylesheet" type="text/css" />
+
+<!---fonts-->
+<link href="css/fontawesome.7.0.1.min.css" rel="stylesheet" type="text/css" />
+<!-- <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet"> -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+<!-- slider -->
+<link href="plugins/owl-carousel/owl.carousel.min.css" rel="stylesheet">
+<link href="plugins/owl-carousel/owl.theme.default.min.css" rel="stylesheet">
+
+<link href="plugins/venobox/venobox.min.css" rel="stylesheet" type="text/css" media="screen" />
+
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link href="css/responsive.css" rel="stylesheet" type="text/css" />
+
+<!-- <script src="js/jquery.3.7.1.min.js"></script> -->
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+<header>  
+
+<div class="container">
+    <div class="logo">
+        <a href="index.php"><img src="images/logo.png" alt="" /></a>
+    </div>
+    <nav>
+        <!-- Menu Toggle btn-->
+        <div class="menu-toggle">
+            <h3>Menu</h3>
+            <button type="button" id="menu-btn">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <!-- Responsive Menu Structure-->
+        <!--Note: declare the Menu style in the data-menu-style="horizontal" (options: horizontal, vertical, accordion) -->
+        <ul id="respMenu" class="ace-responsive-menu" data-menu-style="horizontal">
+            <li><a href="#">Process</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Reviews</a></li>
+            <li><a href="#">About</a></li>
+        </ul>
+    </nav>
+</div>
+</header>
+<!-- End of Responsive Menu -->
+
+
+<div id="main">
+  
+<section class="hero">
+    <!-- <div class="img_box">
+        <img src="images/banner.jpg" alt="" title="">
+    </div> -->
+    <div class="contain_90">
+        <div class="inner_container">
+            
+            <div class="text_box">
+                <div class="line_1">Your First Step to Better<br> Health Starts Here.</div>
+                <div class="line_2">Your first personalised nutrition<br> consultation absolutely free.</div>
+            </div>
+            <div class="lady_overlay">
+                <img src="images/lady-with-fruits.png">
+            </div>
+        </div>
+    </div>
+</section>
+<!-- hero -->
+
+<section class="section_2">
+    <div class="contain_80">
+        <div class="inner_container">
+            
+            <div class="text_wrapper">
+                <p>If you’re struggling with diabetes, PCOS, metabolic syndrome, fatty liver,
+                anemia, or struggling with weight and lifestyle concerns, nutrition can help
+                your body respond better to treatment.</p>
+                <p>At <b>Nutrition With Vibha</b>, we believe healing works best when <b>food and
+                medicine work together.</b></p>
+                <p><b><i>This free consultation is a calm, judgement free space to:</i></b></p>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- section_2 -->
+
+</div>
+<!-- main -->
+
+
+
+<!--sticky header-->
+<!-- <script src="js/classie.js"></script>
+<script>
+function init() {
+window.addEventListener('scroll', function(e){
+  var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+  shrinkOn = 50,
+  header = document.querySelector("header");
+  if (distanceY > shrinkOn) {
+  classie.add(header,"smaller");
+  } else {
+  if (classie.has(header,"smaller")) {
+    classie.remove(header,"smaller");
+  }
+  }
+});
+}
+window.onload = init();
+</script> -->
+<script src="js/common.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+
+
+<script src="js/ace-responsive-menu.js"></script>
+<script>
+$(document).ready(function () {
+    $("#respMenu").aceResponsiveMenu({
+        resizeWidth: '768', // Set the same in Media query       
+        animationSpeed: 'fast', //slow, medium, fast
+        accoridonExpAll: false //Expands all the accordion menu on click
+    });
+});
+</script>
+
+<script src="plugins/venobox/venobox.min.js"></script>
+<script>
+$(document).ready(function(){
+    new VenoBox({
+        selector: '.new_veno',
+        // numeration: true,
+        // infinigall: true,
+        // share: true,
+        // spinner: 'rotating-plane'
+    });
+});
+</script>
+
+<script src="js/wow.min.js"></script>
+<script>
+new WOW().init();
+</script>
+
+<!--slider-->
+<script src="plugins/owl-carousel/owl.carousel.js"></script>
+<script src="plugins/owl-carousel/owl-content-animation.js"></script>
+
+</body>
+</html>
